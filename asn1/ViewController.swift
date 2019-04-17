7f740c410637b5dd303102a8448a303f35b6f8b3
Zinko Vyacheslav <zslavman@gmail.com>
2019-04-17 22:00:19 +0300

    Initial Commit

---
+++ asn1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  asn1
+//
+//  Created by Zinko Viacheslav on 17.04.2019.
+//  Copyright © 2019 Zinko Viacheslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
